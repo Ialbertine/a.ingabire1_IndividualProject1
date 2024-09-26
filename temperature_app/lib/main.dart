2920cfb05d77cb8af1886fae01ccc1852b5e1c2c
Ialbertine <a.ingabire1@alustudent.com>
2024-09-26 22:03:40 +0200

    temperature converter

--- temperature_app/lib/main.dart
+++ temperature_app/lib/main.dart
@@ -1,14 +1,32 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'temperature.dart';  
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class TemperatureApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 8, 174, 14),
+          title: Center(
+            child: Text('Temperature Converter',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+        ),
+        body: Center(child: TemperatureScreen()),
+      ),
+    );
   }
+}
+
+void main() {
+  runApp(TemperatureApp());
 }
